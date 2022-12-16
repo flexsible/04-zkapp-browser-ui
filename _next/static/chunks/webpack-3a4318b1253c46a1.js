21cc86a6a49d1ab635a8286113c1937bbd044383
rsyamsul <rifqism88@gmail.com>
2022-12-15 23:12:43 -0800

    Deploy gh-pages

--- _next/static/chunks/webpack-3a4318b1253c46a1.js
+++ _next/static/chunks/webpack-3a4318b1253c46a1.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "b65fc2553e4c1a06" + ".css";
+/******/ 			return "static/css/" + "27d0fb19eba33876" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
