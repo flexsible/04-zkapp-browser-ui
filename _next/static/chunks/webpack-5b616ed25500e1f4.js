5100aa902de8cbe0a9aec1d628e176e2d50ba26e
rsyamsul <rifqism88@gmail.com>
2022-12-15 22:47:01 -0800

    Deploy gh-pages

--- _next/static/chunks/webpack-5b616ed25500e1f4.js
+++ _next/static/chunks/webpack-5b616ed25500e1f4.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "0c9d0f76daac8700" + ".css";
+/******/ 			return "static/css/" + "b65fc2553e4c1a06" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
